185fb61f28b75f920e33060bff8d2b6c369c1ca8
KenQui3312 <kenetadiel@gmail.com>
2024-11-29 07:17:27 -0600

    modificacion 29-11-24 7:00

---
+++ js/cel.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    const menu = document.querySelector('.navbar ul');
+    menu.classList.toggle('show'); // Añadir o quitar la clase 'show' para mostrar/ocultar el menú
+}
